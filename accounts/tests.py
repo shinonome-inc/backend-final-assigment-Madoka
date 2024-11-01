9afaa708ae9208d37ed269d8213c39288867d5ba
Madoka <m.shimohata.24.3.1296@4nonome.com>
2024-11-02 00:14:10 +0900

    テストコード

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,60 @@
+from django.contrib.auth import SESSION_KEY, get_user_model
+from django.test import TestCase
+from django.urls import reverse
+
+User = get_user_model()
+
+
+class TestSignupView(TestCase):
+    def setUp(self):
+        self.url = reverse("accounts:signup")
+
+    def test_success_get(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "accounts/signup.html")
+
+    def test_success_post(self):
+        valid_data = {
+            "username": "testuser",
+            "email": "test@test.com",
+            "password1": "testpassword",
+            "password2": "testpassword",
+        }
+
+        response = self.client.post(self.url, valid_data)
+
+        # 1の確認 = tweets/homeにリダイレクトすること
+        self.assertRedirects(
+            response,
+            reverse("tweets:home"),
+            status_code=302,
+            target_status_code=200,
+        )
+        # 2の確認 = ユーザーが作成されること
+        self.assertTrue(User.objects.filter(username=valid_data["username"]).exists())
+        # 3の確認 = ログイン状態になること
+        self.assertIn(SESSION_KEY, self.client.session)
+
+    def test_failure_post_with_empty_username(self):
+        invalid_data = {
+            "username": "",
+            "email": "test@test.com",
+            "password1": "testpassword",
+            "password2": "testpassword",
+        }
+        response = self.client.post(self.url, invalid_data)
+        form = response.context["form"]
+
+        print(response.context)
+        print(response.context["form"])
+        print(form.errors)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertFalse(User.objects.filter(username=invalid_data["username"]).exists())
+        self.assertFalse(form.is_valid())
+        self.assertIn("このフィールドは必須です。", form.errors["username"])
+
 # from django.test import TestCase
 
 
